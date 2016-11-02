3abf16e03db1b23863901092623bee048faa74e4
bilouwan <moroine.bentefrit@adactive.com>
2016-11-02 18:23:25 +0100

    Fix import !

--- import.js
+++ import.js
@@ -1,146 +1,103 @@
+const CryptoJS = require("crypto-js");
+const XLSX = require("xlsx");
+const restify = require("restify-clients");
 
-var fs = require('fs');
-var XLSX = require('xlsx');
-var ACA = require('./vendor/adsum-client-api.es5.js')
+const username = '473-device';
+const key = 'c43de14913b4173c07cd7229fbbc45913129078ee9babbfc81dc341c49b15f24';
+const endpoint = 'https://preprod-api.adsum.io';
 
+const _client = restify.createJsonClient(endpoint);
+const _obj = {};
 
-var restify = require('restify-clients');
-var wsse = require('wsse');
+const createWSSEHeader = () => {
+    let now = new Date();
+    let created = parseInt(now.getTime() / 1000); // Timestamp in seconds
+    let nonce = CryptoJS.MD5(parseInt(Math.random() * 100000).toString());
 
-var _em = null;
-var _obj = {};
-var _errors = [];
+    let digest = CryptoJS.SHA1(nonce + created + key);
 
+    return 'UsernameToken Username="' + username + '", PasswordDigest="' + digest + '", Nonce="' + nonce + '", Created="' + created + '"';
+};
 
-var _token = wsse({ username: '473-device', password: 'fc836794f5a4e2d0a3416d360e88edda2c3e1227cc6462f8c1f8b619bb51a26d' });
-var _client = restify.createJsonClient({url: "http://preprod-api.adsum.io/api/1.1"});
+const postData = (path, data, callback) => {
+    _client.post({
+            path: path,
+            headers: {
+                "AUTHORIZATION": 'WSSE profile="UsernameToken"',
+                "X-WSSE": createWSSEHeader()
+            }
+        }, data,
+        callback
+    );
+};
 
+const postPoi = (obj) => {
+    postData('/2.1/poi', obj, (err, req, res) => {
+            if (err) {
+                console.log("POST KO : " + err)
+            } else {
+                console.log("POST OK " + obj.name)
+            }
+        }
+    );
+};
 
+const postCategory = (obj) => {
+    postData('/2.1/category', obj, (err, req, res) => {
+            if (err) {
+                console.log("POST KO : " + err)
+            } else {
+                console.log("POST OK " + obj.name)
+            }
+        }
+    );
+};
 
+const execLine = (obj) => {
+    if (obj["Business"] != undefined) {
+        const cat = {};
+        cat.type = 'category';
+        cat.name = obj["Business"];
+        postCategory(cat);
+    }
+    if (obj["Shop Name"] != undefined) {
+        const poi = {};
+        poi.type = 'poi';
+        poi.name = obj["Shop Name"];
+        poi.place = obj["Unit No"];
+        poi.category = obj["Business"];
+        postPoi(poi);
+    }
+};
 
-//postPoi({name:"hello"})
-parseXlsx(execLine);
-for(var i in _obj["category"])
-{
-	
-	console.log(i);
+const parseXlsx = () => {
+    const workbook = XLSX.readFile('data/FE/FE.xlsx');
 
-}
+    const sheet_name_list = workbook.SheetNames;
 
+    for (const y of sheet_name_list) {
+        var worksheet = workbook.Sheets[y];
+        var headers = {};
+        var data = [];
 
-/*
-init()
-{
+        for (const z in worksheet) {
+            if (z[0] === '!') continue;
+            //parse out the column, row, and value
+            var col = z.substring(0, 1);
+            var row = parseInt(z.substring(1));
+            var value = worksheet[z].v;
 
-	var options = new ACA.Options();
-	options.endpoint = "https://api.adsum.io"; // The Adsum Api Endpoint
-	options.site = 369; // The site id to retrieve data from
-	options.username = "473-device"; // The Adsum Api username, on format "{id}-device" with *id* the deviceId attributed to the application.
-	options.key = "fc836794f5a4e2d0a3416d360e88edda2c3e1227cc6462f8c1f8b619bb51a26d"; // The Device Api Key
+            //store header names
+            if (row == 1) {
+                headers[col] = value;
+                continue;
+            }
 
-	var em = new ACA.EntityManager(options);
+            if (!data[row]) data[row] = {};
+            data[row][headers[col]] = value;
+            execLine(data[row]);
+        }
+    }
+};
 
-	// Load data asynchronously using Promise
-	em.load().then(function(){
-	    // Data loaded
-
-	    // Get the Poi repository
-	    var repository = em.getRepository("Poi");
-
-	    // Get all Pois
-	    var pois = repository.getAll();
-	}, function(error){
-	    // Handle the error
-	});
-}*/
-
-function postPoi(obj)
-{
-	_client.post('/poi',obj, (err, req, res) =>{
-		if(err)
-		{
-			console.log("POST KO : "+err)
-		}else{
-		  console.log("POST OK " +obj.name)
-		}
-	});
-}
-
-
-
-function importAdsumData()
-{
-
-	    // // Get the Poi repository
-	    // var repository = em.getRepository("Poi");
-	    // // Get all Pois
-	    // var pois = repository.getAll();
-
-}
-
-
-
-
-
-
-function execLine(obj)
-{
-	  if(obj["Business"] != undefined)
-	  {
-		var cat =   {};
-		cat.type = 'category';
-		cat.name = obj["Business"];
-		addObject(cat);
-	  }
-	  if(obj["Shop Name"] != undefined)
-	  {
-		var poi =   {};
-		poi.type = 'poi';
-		poi.name = obj["Shop Name"];
-		poi.place = obj["Unit No"];
-		poi.category = obj["Business"];
-		addObject(poi);
-	  }
-}
-
-function addObject(obj)
-{
-	if(_obj[obj.type] == undefined)
-	{
-		_obj[obj.type] = [];
-	}
-
-	_obj[obj.type][obj.name] = obj;
-}
-
-function parseXlsx()
-{
-	var workbook = XLSX.readFile('data/FE/FE.xlsx');
-
-	var sheet_name_list = workbook.SheetNames;
-	sheet_name_list.forEach(function(y) {
-	    var worksheet = workbook.Sheets[y];
-	    var headers = {};
-	    var data = [];
-	    for(z in worksheet) {
-	        if(z[0] === '!') continue;
-	        //parse out the column, row, and value
-	        var col = z.substring(0,1);
-	        var row = parseInt(z.substring(1));
-	        var value = worksheet[z].v;
-
-	        //store header names
-	        if(row == 1) {
-	            headers[col] = value;
-	            continue;
-	        }
-
-	        if(!data[row]) data[row]={};
-	        data[row][headers[col]] = value;
-	        execLine(data[row]);
-	    }
-	    //drop those first two rows which are empty
-  
-	});
-}
-
+parseXlsx();
